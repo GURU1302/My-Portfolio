073da660ee3d7f8a205b8d15f0b0af2825af1050
PIYUSH GHOSH <impiyushghosh@gmail.com>
2023-03-27 03:05:38 +0530

    added the testimonial , project with me and the footer section

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,9 +78,11 @@ modalCloses.forEach((modalClose) => {
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-let swiper = new Swiper('.portfolio__container', {
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
+
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -92,7 +94,22 @@ let swiper = new Swiper('.portfolio__container', {
     });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
 
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+    });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
